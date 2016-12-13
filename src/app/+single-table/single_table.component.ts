25dedb17616cb7a2b5f9178732fe5af1106e7bd1
Albert Pałka <albert@codequest.com>
2016-12-13 18:50:32 +0100

    A lot has changed again + single table page

---
+++ src/app/+single-table/single_table.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+/*
+ * We're loading this component asynchronously
+ * We are using some magic with es6-promise-loader that will wrap the module with a Promise
+ * see https://github.com/gdi2290/es6-promise-loader for more info
+ */
+
+console.log('`SingleTableComponent` component loaded asynchronously');
+
+@Component({
+  // The selector is what angular internally uses
+  // for `document.querySelectorAll(selector)` in our index.html
+  // where, in this case, selector is the string 'home'
+  selector: 'single-table',  // <home></home>
+  // We need to tell Angular's Dependency Injection which providers are in our app.
+  // Our list of styles in our component. We may add more to compose many styles together
+  styleUrls: [ './single_table.component.css' ],
+  // Every Angular template is first compiled by the browser before Angular runs it's compiler
+  templateUrl: './single_table.component.html'
+})
+
+export class SingleTableComponent {
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    console.log('hello `single-table` component');
+  }
+
+}
