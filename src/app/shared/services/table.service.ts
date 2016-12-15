02c8f62811ce98065ee6cdff834e38773005387f
Albert Pałka <albert@codequest.com>
2016-12-15 17:02:50 +0100

    komituję

--- src/app/shared/services/table.service.ts
+++ src/app/shared/services/table.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, RequestOptions, Headers } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 
 import * as moment from 'moment';
@@ -149,8 +149,11 @@ export class TableService {
     }
 
     createTable(user_name:string, zone_id:string) {
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        let options = new RequestOptions({ headers: headers });
         return this.http.post('http://192.168.1.144:4000/game_sessions',
-            JSON.stringify({user_name, zone_id}))
+            JSON.stringify({user_name, zone_id}),
+            options)
             .map(res => res.json());
     }
 }
