73f7452520d8f1f272bd6a4f0a0a8cdb28f838c0
AdamMankowski <adam.mankowski@codequest.com>
2016-12-15 10:51:26 +0100

    header login

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../user.service';
 
 @Component({
     selector: 'kot-header',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-    constructor() { }
+    constructor(private user:User) { 
+        this.user2 = this.user
+    }
+    user2 = {}
 
     ngOnInit() { }
+    
 }
